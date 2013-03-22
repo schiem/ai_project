f4519272b8261771cbae4afcdbf7f1a50416cc88
Michael John Yoder <mjyoder@un004-imini06.local>
2013-03-22 13:53:58 -0400

    Added the main file and a base class for monster and player. Mostly empty. new file:   main.py new file:   monster.py

---
+++ monster.py
@@ -0,0 +1,12 @@
+class Monster:
+    def __init__(health=100, name="Darkling", offbalance=False):
+        self.health = health
+        self.name = name
+
+    def attack(self):
+        #calculate and return the best move
+
+    def take_damage(self, damage):
+        pass
+
+class Player(Monster)

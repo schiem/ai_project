69bf957ce3a9a5fb50436c8a95327d2078bd1036
Michael John Yoder <mjyoder@un004-imini03.local>
2013-04-03 13:54:00 -0400

    Began implementing the main, creating a user interface object and a main player.  It also has commented pseudo-code for how the rest of the world implementation will go.  Removed the player subclass from the mosnters.
    main.py
    monster.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+import monster
+import ui
+
+if __name__ == '__main__':
+    running = True
+    player = Monster(100, 'temp_name')
+    user_interface = UI(player)
+    while running == True:
+        pass
+        #player_move = UI.do_something()
+        #monster_move = monster.attack()
+        #monster.do_action(player_move, monster_move)
+        #check_if_dead()
+
 
--- monster.py
+++ monster.py
@@ -2,11 +2,11 @@ class Monster:
     def __init__(health=100, name="Darkling", offbalance=False):
         self.health = health
         self.name = name
-
+        self.offbalance = offbalance
     def attack(self):
         #calculate and return the best move
+        return 'attack'        
 
     def take_damage(self, damage):
-        pass
+        self.health -= damage
 
-class Player(Monster)
